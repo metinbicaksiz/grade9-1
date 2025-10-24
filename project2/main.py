5bf04153154df27313b9dc60893427f2022af700
Metin Bicaksiz <metin@gobindsarvar.ca>
2025-10-24 13:20:25 -0700

    projects 1-4

---
+++ project2/main.py
@@ -0,0 +1,17 @@
+# Learn to use string concatenation and the new line escape sequence to format strings in Python.
+#
+# PAUSE 1. Use \n to add another line of "Hello world".
+# So the resulting output looks like this:
+#
+# Hello world!
+#
+# Hello world!
+#
+# Hello world!
+#
+# PAUSE 2. Add a space between the strings
+# So there is a space between the string Hello and Metin when the print statement runs.
+#
+# The output should look like this:
+#
+# Hello Metin
